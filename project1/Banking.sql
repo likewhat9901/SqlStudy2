c44f9de7123154c2febd2163baa91db5942b7dd7
likewhat9901 <parkko123@naver.com>
2025-04-17 16:47:02 +0900

    JDBC 완료

--- project1/Banking.sql
+++ project1/Banking.sql
@@ -18,9 +18,37 @@ create sequence seq_banking_idx
 commit;
     
 select * from banking;
+
+delete from banking;
+
+
+--삭제 프로시저
+create or replace procedure DeleteAccount
+    (
+        p_id in varchar2,
+        p_result out varchar2
+    )
+is
+    v_count number;
+begin
+    v_count := SQL%rowcount;
     
+    if v_count = 0 then
+        p_result := '해당아이디는 존재하지 않습니다.';
+    else
+        delete from banking
+        where acc_id = p_id;
+        
+        p_result := '삭제가 완료되었습니다.';
+    end if;
     
-    
+EXCEPTION
+    when others then
+        p_result := '오류 발생: ' || SQLERRM;
+end;
+/
+
+commit;
     
     
     
